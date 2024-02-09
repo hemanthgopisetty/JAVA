1888bae5bcee12355d37327cd8d3dd763ee2a788
hemanthgopisetty <hemanth.508136@gmail.com>
2024-02-09 15:34:57 +0530

    Block

---
+++ BlockText.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+class BlockText {
+ 
+    public static void main(String[] args) {
+        int x,y;
+
+        y=20;
+
+        for(x=0;x<y;x++)
+        {// begin a block
+            System.out.println("This is x: " + x); 
+            System.out.println("This is y: " + y); 
+            y = y - 2;
+
+        }//end of a block
+    }
+    
+ }
